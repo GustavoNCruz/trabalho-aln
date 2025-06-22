f3e2d5aadaa10e8818fbf19e8bf684ff0f88f3db
francisco <franciscoa@ufmg.br>
2025-06-22 17:01:38 -0300

    conserta geração aleatória para doubles

--- gen.cpp
+++ gen.cpp
@@ -1,47 +1,52 @@
 #include <bits/stdc++.h>
+#include <limits>
+#include <random>
 using namespace std;
 
+int main()
+{
 
-int main(){
+    while (1)
+    {
+        int n = 1000;
+        int m = 300000;
 
+        vector<tuple<int, int, double>> arestas;
+        vector<vector<int>> g(n);
+        uniform_real_distribution<double> unif(1e-8, 1e2);
+        default_random_engine r_eng;
+        for (int i = 0; i < m; i++)
+        {
+            int u, v;
+            u = rand() % n;
+            v = rand() % n;
+            double d = unif(r_eng);
+            g[u].push_back(v);
+            g[v].push_back(u);
+            arestas.push_back({u, v, d});
+        }
 
-	while(1){
-		int n = 1000;
-		int m = 300000;
+        vector<int> marc(n);
+        int tot = 0;
+        auto dfs = [&](auto &&self, int u) -> void {
+            marc[u] = 1;
+            tot++;
+            for (auto v : g[u])
+                if (!marc[v])
+                    self(self, v);
+        };
+        dfs(dfs, 0);
 
-		vector<tuple<int,int,double>> arestas;
-		vector<vector<int>> g(n);
-		for(int i=0;i<m;i++){
-			int u, v;
-			u = rand() % n;
-			v = rand() % n;
-			double d = (rand() % 100 + 1);
-			//double d = 1;
-			//cout << u << " " << v << " " << d << endl;
-			g[u].push_back(v);
-			g[v].push_back(u);
-			arestas.push_back({u, v, d});
-		}
+        if (tot != n)
+        {
+            continue;
+        }
 
-		vector<int> marc(n);
-		int tot = 0;
-		auto dfs = [&] (auto&& self, int u) -> void{
-			marc[u] = 1;
-			tot++;
-			for(auto v : g[u]) if(!marc[v]) self(self, v);
-		};
-		dfs(dfs, 0);
+        cout << n << " " << m << endl;
+        for (auto [i, j, x] : arestas)
+            cout << i + 1 << " " << j + 1 << " " << x << endl;
 
-		if(tot != n){
-			continue;
-		}
-
-		cout << n << " " << m << endl;
-		for(auto [i, j, x] : arestas) cout << i+1 << " " << j+1 << " " << x << endl;
-
-
-		cout << 1 << " " << 2 << endl;
-		return 0;
-	}
-	
+        cout << 1 << " " << 2 << endl;
+        return 0;
+    }
 }
